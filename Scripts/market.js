e051289e038ed37d4b8550d6849f9703aedc39cb
lipingyang <1401271223@qq.com>
2016-11-25 18:13:57 +0800

    Update market.js

--- Scripts/market.js
+++ Scripts/market.js
@@ -5,20 +5,10 @@ app.factory("dataModel", function ($http) {
     var factory = {};
     factory.historicalQuote = [];
     factory.productList = [];
+    factory.TempList = [];
     factory.signalRDat = [];
-    factory.returnData = function (data, name) {
-        if (name == "data") {
-            factory.historicalQuote = data;
-        }
-        else if (name == "productList") {
-            factory.productList = data
-        }
-        else if (name == "signalRData") {
-            factory.signalRData = data 
-            console.log(factory.signalRData)
-        }
-    }
-    
+    factory.symbolToIndex = {}; //存储字典
+    //获取getSignalR
     factory.getSignalR = function (symbol) {
         var connection = $.hubConnection('http://114.55.146.142:801/signalr', {
             useDefaultPath: false
@@ -26,10 +16,13 @@ app.factory("dataModel", function ($http) {
         var proxy = connection.createHubProxy('forexHub');
         proxy.on("onNewBasicProductInfoStr", function (symbol, data) {
             if (data != '*') {
-                setTimeout(function () {
-                    factory.signalRData = data
-                },1000)
-                
+
+                mdata = data.split("-");
+                // console.log(data);
+                //console.log(factory.symbolToIndex[mdata[0]]);
+                //console.log(factory.TempList);
+                factory.TempList[factory.symbolToIndex[mdata[0]]].price = mdata[7];
+                factory.signalRData = data
             }
         });
         connection.start().done(function () {
@@ -41,7 +34,7 @@ app.factory("dataModel", function ($http) {
     }
     //获取tempList  datamodel
     factory.getProductList = function () {
-        $http({
+        return $http({
             url: 'http://114.55.146.142:801/api/Data/GetProductMetalist',
             method: 'POST',
             data: {
@@ -50,12 +43,27 @@ app.factory("dataModel", function ($http) {
             },
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
         }).success(function (data) {
-            factory.returnData(data, "productList")
+            factory.productList = data;
         }).error(function () {
             console.log("error")
         });
     }
 
+    //获取tempList  组建下标字典,并循环订阅一次
+    factory.getTempList = function (data) {
+        var tempList = [];
+        // symbolToIndex = {};
+        for (var i = 0; i < data.length; i++) {
+            // console.log(i);
+            tempList[i] = { price: "0", name: data[i].Symbol, display: data[i].SymbolDisplay, color: "" };
+            factory.symbolToIndex[data[i].Symbol] = i;
+            //和服务端建立连接，通过SignalR有数据是给予推送
+            // factory.getSignalR(data[i].Symbol);
+        }
+        factory.TempList = tempList;
+        return tempList;
+    }
+    //历史数据
     factory.getData = function (symbol, interval) {
         $http({
             url: 'http://114.55.146.142:801/api/Data/GetHistoricalQuote',
@@ -80,7 +88,7 @@ app.factory("dataModel", function ($http) {
                     data.Close[i]
                 ])
             }
-            factory.returnData(ohlcData, "data")
+            factory.historicalQuote = ohlcData;
         }).error(function () {
             console.log("error")
         });
@@ -93,50 +101,59 @@ app.controller('MarketController', function ($scope, $http, $timeout, dataModel)
     $scope.dataModel = dataModel;
 
     dataModel.getProductList();
-    var store = this;
-    $scope.$watch('dataModel.productList', function (newData, oldData) {
-        if (newData.length != 0) {
-            console.log('getProductList is successed')
-            var tempList = [];
-            symbolToIndex = {};
-            for (var i = 0; i < newData.length; i++) {
-                tempList[i] = { price: "0", name: newData[i].Symbol, display: newData[i].SymbolDisplay, color: "" };
-                symbolToIndex[newData[i].Symbol] = i;
-            }
-            store.products = tempList;
 
-            var signalRSymbol = [];
-            for (var i = 0; i < tempList.length; i++) {
-                signalRSymbol.push(tempList[i].name)
-            }
-            console.log(signalRSymbol)
-            signalRSymbol = signalRSymbol.join(";")
-            console.log(signalRSymbol)
-            dataModel.getSignalR(signalRSymbol);
-        } else {
-            console.log('getProductList is falied')
+    //调用服务，获得列表数据，并进行绑定
+    dataModel.getProductList().success(function (data) {
+        var tempList = dataModel.getTempList(data);
+        $scope.products = tempList;
+        var signalRSymbol = [];
+        for (var i = 0; i < tempList.length; i++) {
+            signalRSymbol.push(tempList[i].name)
         }
+        console.log(signalRSymbol)
+        signalRSymbol = signalRSymbol.join(";")
+        console.log(signalRSymbol)
+        dataModel.getSignalR(signalRSymbol);
 
-    }, true)
+    });
+
+    $scope.$watch('$scope.price', function (n, o) {
+        if (n > o) {
+            console.log('big')
+        } else {
+            console.log('small')
+        }
+    })
+
     //获取signalR数据 datamodel
     $scope.$watch('dataModel.signalRData', function (newData, oldData) {
+        var symbolToIndex = dataModel.symbolToIndex;
+        if (newData > oldData) {
+            console.log('big')
+        }
+
+
+
         if (newData != undefined) {
             data = newData;
             data = data.split("-");
+            console.log(data);
             var symbol = data[0];
-            var oldprice = store.products[symbolToIndex[symbol]].price;
-            store.products[symbolToIndex[symbol]].price = data[7];
-            var newprice = store.products[symbolToIndex[symbol]].price;
+            var oldprice = $scope.products[symbolToIndex[symbol]].price;
+            console.log(oldprice)
+            $scope.products[symbolToIndex[symbol]].price = data[7];
+            var newprice = $scope.products[symbolToIndex[symbol]].price;
+            console.log(newprice)
             if (oldprice == newprice) {
 
             } else if (oldprice > newprice) {
-                store.products[symbolToIndex[symbol]].color = "blue"
+                $scope.products[symbolToIndex[symbol]].color = "blue"
             } else {
-                store.products[symbolToIndex[symbol]].color = "red"
+                $scope.products[symbolToIndex[symbol]].color = "red"
             }
             $timeout(
                 function () {
-                    store.products[symbolToIndex[symbol]].color = ""
+                    $scope.products[symbolToIndex[symbol]].color = ""
                 },
                 2000
             );
