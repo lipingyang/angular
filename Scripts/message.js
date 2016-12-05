a5ba077f4edc2d4b74c1a51d82b9ec280a9d5041
lipingyang <1401271223@qq.com>
2016-12-05 18:18:08 +0800

    Update message.js

--- Scripts/message.js
+++ Scripts/message.js
@@ -1,39 +1,75 @@
 (function() {
-    var message = angular.module('message', ['ngRoute']);
-    message.config(function ($routeProvider) {
-        $routeProvider.when('/important', {
-            templateUrl: '/App/news/important.html'
-        }).when('/push', {
-            templateUrl: '/App/news/push.html'
-        }).when('/live', {
-            templateUrl: '/App/news/live.html',
-        }).when('/foreign', {
-            templateUrl: '/App/news/foreign.html',
-        }).when('/',{
-            templateUrl: '/App/news/important.html'
-        })
+    var message = angular.module('Message', []);
+    message.factory('MessageModel', function ($http) {
+      
+        //获得菜单目录
+        var factory = {};
+        factory.GetMenulist = function () {
+           return $http({
+                url: 'http://114.55.146.142:801/api/Data/GetNewsMeta',
+                method: 'GET',
+           }).success(function (data) {
+               return data;
+                }).error(function () {
+                    console.log("error")
+                });
+        }
+
+        //获得列表内容目录
+        factory.ListData = function (CateId = 'USA') {
+            console.log(CateId);
+            var stattime = (Date.parse(new Date()))/1000;
+            var begintime = (Number(stattime) - Number(48 * 3600)).toString();
+            var endtime = stattime.toString();
             
+            console.log(begintime, endtime);
+            return $http({
+                url: 'http://114.55.146.142:801/api/Data/GetNewsBaseList',
+                method: 'POST',
+                data: {
+                    CategoryId: CateId,
+                    StartTime: begintime,
+                    EndTime: endtime,
+                    MaxCount: 20
+                },
+                headers: { 'Content-Type': 'application/json' }
+            }).success(function (data, status, headers, config) {
+               
+            }).error(function (data, status, headers, config) {
+                console.log("error...");
+            });
+        }
+
+        return factory;
+
+       
     });
-	message.controller('MessageController', function(){
-		this.newlist = newlists;
-		this.rightlist = rightlists;
-	});
-	
-	var newlists = [
-	    {four:'商业银行到了最危险的时候1',five:'什么是“资产脱媒”？“资产脱媒”过程，是指商业银行资产配置已经开始 脱离商业银行传统1',six:'阅读全文1',read:'121',data:'2015-7-061',img:'../images/list-img.png'},
-	    {four:'商业银行到了最危险的时候2',five:'什么是“资产脱媒”？“资产脱媒”过程，是指商业银行资产配置已经开始 脱离商业银行传统2',six:'阅读全文1',read:'121',data:'2015-7-061',img:'../images/list-img.png'},
-	    {four:'商业银行到了最危险的时候3',five:'什么是“资产脱媒”？“资产脱媒”过程，是指商业银行资产配置已经开始 脱离商业银行传统3',six:'阅读全文1',read:'121',data:'2015-7-061',img:'../images/list-img.png'},
-	    {four:'商业银行到了最危险的时候4',five:'什么是“资产脱媒”？“资产脱媒”过程，是指商业银行资产配置已经开始 脱离商业银行传统4',six:'阅读全文1',read:'121',data:'2015-7-061',img:'../images/list-img.png'}
-	    ];
-	
-	var rightlists=[
-	   {img:'../images/hotNews.png',txt:'中国债务问题或许没你想的这么'},
-	   {img:'../images/hotNews.png',txt:'中国债务问题或许没你想的这么'},
-	   {img:'../images/hotNews.png', txt: '中国债务问题或许没你想的这么' },
-	   {img: '../images/hotNews.png', txt: '中国债务问题或许没你想的这么' },
-	   {img: '../images/hotNews.png', txt: '中国债务问题或许没你想的这么' },
-	   {img: '../images/hotNews.png', txt: '中国债务问题或许没你想的这么' }
-	];
+   
+    //控制器模块，实现数据绑定
+    message.controller('MessageController',function($scope,$http,MessageModel){
+        //绑定menu
+        console.log(MessageModel.GetMenulist());
+         MessageModel.GetMenulist().success(function (data,status) {
+             $scope.Menu = data;
+             console.log(data)
+             console.log(status);
+         });
+        //新闻列表模块获得数据并实现绑定
+         $scope.ListData = MessageModel.ListData().success(function (data) {
+             $scope.ListData = data;
+             // console.log(data);
+         });
+         
+         $scope.GetList = function (CateId) {
+             MessageModel.ListData(CateId).success(function (data) {
+                 $scope.ListData = data;
+                 console.log(data);
+             });
+         };
+        
+    
+    });
+
 
 
 	
